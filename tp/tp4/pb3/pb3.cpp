98e176caa8086bfb35e076c652804cd65e12438e
Mehdi <ougadiofficial@gmail.com>
2022-09-28 11:18:30 -0400

    Mehdi: Tp4 finis

---
+++ tp/tp4/pb3/pb3.cpp
@@ -0,0 +1,99 @@
+#define F_CPU 8000000
+
+#include <util/delay.h>
+#include "avr/io.h"
+#include <avr/interrupt.h>
+
+//Constante de Delay
+const uint8_t delay= 5;
+
+//Initialisation des entrées et sorties
+void setIOPorts()
+{
+    //Mode input
+    DDRD &= ~( 1<< PD2);
+
+    //Mode output
+    DDRA |= ((1<< PA1) | (1<< PA0));
+
+    PORTA = 0x00; 
+
+}
+
+//Le button a été appuyé
+bool buttonPressed()
+{
+    if (PIND & 0x04)
+    {
+        _delay_ms(delay);
+        return (PIND & 0x04);
+    }
+    return false;
+}
+
+//Liaison entre le port et les couleurs
+enum class color{red, green, amber, closed};
+
+void setDelColor(color color)
+{
+    switch (color)
+    {
+        case color::green:
+            PORTA |= (1<<PA0);
+            PORTA &= ~(1<<PA1);
+            break;
+
+        case color::red:
+            PORTA |= (1<<PA1);
+            PORTA &= ~(1<<PA0);
+            break;
+
+        case color::amber:
+
+            setDelColor(color::green);
+            _delay_ms(delay*2);
+            setDelColor(color::red);
+            _delay_ms(delay-4);
+
+            break;
+
+        case color::closed:
+            PORTA &= ~((1<<PA0) | (1<<PA1));
+            break;
+    }
+}
+
+void ajustementPwm ( uint8_t valeurPWM ) {
+
+// mise à un des sorties OC1A et OC1B sur comparaison
+// réussie en mode PWM 8 bits, phase correcte
+// et valeur de TOP fixe à 0xFF (mode #1 de la table 16-5
+// page 130 de la description technique du ATmega324PA)
+
+OCR1A = 0;
+OCR1B = valeurPWM;
+
+// division d'horloge par 8 - implique une fréquence de PWM fixe
+
+TCCR1A |= (1<<WGM10)|(1<<COM1A1)|(1<<COM1B1);
+TCCR1B |= (1<< CS11); //prescaler à 8
+TCCR1C |= 0;
+
+}
+
+int main()
+{
+    setIOPorts();
+
+    ajustementPwm(0);
+    _delay_ms(2000);
+    PORTA=0x01;
+    ajustementPwm(64);
+    _delay_ms(2000);
+    ajustementPwm(128);
+    _delay_ms(2000);
+    ajustementPwm(191);
+    _delay_ms(2000);
+    ajustementPwm(255);
+    _delay_ms(2000);
+}
